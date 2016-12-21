0338f73d07d22830ef63940d842dcd3fbb529e85
Labrador <mlabesp@gmail.com>
2016-12-21 20:17:16 +0100

    Se añade el segundo archivo

---
+++ Operacion2.py
@@ -0,0 +1,13 @@
+def operation2(message_v1):
+	print(message_v1+"hola"+"quepasa")
+
+
+
+
+
+
+<<<<<<< HEAD
+operation2('usando git en master')
+=======
+operation2('usando git en experimetnt')
+>>>>>>> experiment
